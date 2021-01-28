4898b19e24f6f419c37b2e5a6311671172952485
RamVegiraju <rav3sa@virginia.edu>
2021-01-28 16:22:15 -0500

    Init commit

---
+++ hello.rb
@@ -0,0 +1,60 @@
+#Install code runner
+#https://stackoverflow.com/questions/54917999/how-do-i-execute-a-ruby-script-using-vscode
+
+#printing to screen
+#puts has same functionality except it prints out a new line afterwards
+#print has everything on the same line
+
+=begin
+
+puts "Test"
+puts "Hello World"
+print "Hello World"
+print " Concatenate"
+
+=end
+
+
+#line by line execution
+=begin
+
+puts "   /"
+puts "  /|"
+puts " /_|"
+puts "/__|"
+
+=end
+
+#variables
+=begin
+
+character_name = "John"
+character_age = "35"
+
+puts ("There once was a man named " + character_name)
+puts ("he was 70 years old")
+#can modify variable val throughout the program
+character_name = "Tom"
+puts ("He really liked the name " + character_name)
+puts ("but didn't like being " + character_age)
+
+=end
+
+#data types
+
+#Strings
+name = "Mike"
+
+#Integers
+age = 75
+
+#Floating Point
+gpa = 3.25
+
+#boolean
+ismale = true
+istall = false
+
+#nil means there is no value
+flaws = nil
+
